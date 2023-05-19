f86c3fcb1e0db2daaa76967319461a5b5441ca71
PracticeKJY <poiijtl1004@naver.com>
2023-05-19 19:20:57 +0900

    ✨ feat. Location 기능 구현
    - leaflet 라이브러리를 사용한 지도, 마커 구현
    - world-countries라이브러리를 이용한 나라 리스트 구현
    - react-select 라이브러리를 이용한 select 창 구현

--- app/@component/Modal/RentModal.tsx
+++ app/@component/Modal/RentModal.tsx
@@ -6,6 +6,10 @@ import { useMemo, useState } from "react"
 import Heading from "../Heading"
 import { categories } from "../Header/Categories"
 import CategoryInput from "../Inputs/CategoryInput"
+import { FieldValues, useForm } from "react-hook-form"
+import CountrySelect from "../Inputs/CountrySelect"
+import MapComponent from "./../MapComponent"
+import dynamic from "next/dynamic"
 
 //enum은 관련된 상수들을 그룹화하고 식별하기 위해 사용됩니다. 특히, enum은 서로 연관된 상수의 집합을 정의하는 데 유용합니다. 이렇게 정의된 enum은 TypeScript 코드에서 해당 상수를 사용할 수 있게 되며, 가독성과 유지보수의 편의성을 높여줍니다.
 //TypeScript에서는 enum 상수에 대한 값을 따로 지정하지 않으면, 0부터 시작하여 순차적인 값(0, 1, 2, ...)이 자동으로 할당됩니다
@@ -22,6 +26,45 @@ const RentModal = () => {
   const rentModal = useRentModal()
   const [step, setStep] = useState(STEPS.CATEGORY)
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  })
+
+  const category = watch("category")
+  const location = watch("location")
+  const MapComponent = useMemo(
+    () =>
+      dynamic(() => import("../MapComponent"), {
+        ssr: false,
+      }),
+    [location],
+  )
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    })
+  }
+
   const onBack = () => {
     setStep((value) => value - 1)
   }
@@ -31,10 +74,10 @@ const RentModal = () => {
 
   const actionLabel = useMemo(() => {
     if (step === STEPS.PRICE) {
-      return "Create"
+      return "생성"
     }
 
-    return "Next"
+    return "다음"
   }, [step])
 
   const secondaryActionLabel = useMemo(() => {
@@ -42,10 +85,10 @@ const RentModal = () => {
       return undefined
     }
 
-    return "Back"
+    return "뒤로"
   }, [step])
 
-  const bodyContent = (
+  let bodyContent = (
     <div
       className="
     flex flex-col gap-8
@@ -63,8 +106,10 @@ const RentModal = () => {
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput
-              onClick={() => {}}
-              selected={false}
+              onClick={(category) => {
+                setCustomValue("category", category)
+              }}
+              selected={category === item.label}
               label={item.label}
               icon={item.icon}
             />
@@ -74,12 +119,32 @@ const RentModal = () => {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div
+        className="
+    flex flex-col gap-8
+    "
+      >
+        <Heading
+          title="어디에 위치하고 있나요?"
+          subtitle="게스트들이 찾을 수 있도록 위치를 알려주세요"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <MapComponent center={location?.latlng} />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       title={"Restay할 준비가 되셨나요?"}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
       secondaryActionLabel={secondaryActionLabel}
